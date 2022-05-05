4e9748f60c89a9b8d8d57f04a13c454ecb5e2f64
Ntaoleng Mohlouoa <mohlouoa23ntaoleng@gmail.com>
2022-05-05 16:08:17 +0200

    task 0.9

---
+++ task_0.8.py
@@ -0,0 +1,20 @@
+def minutes_to_hours(minutes):
+    calc_hours = minutes // 60
+    calc_minutes = minutes % 60
+    if calc_hours == 1:
+        print(str(calc_hours) +" hour", ",", " ", end="")
+    elif calc_hours > 1:
+        print(str(calc_hours) +" hours", ",", " ", end="")
+    elif calc_hours == 0:
+        print(str(calc_hours) + " hours")
+    if calc_minutes == 1:
+        print(str(calc_minutes) +" minute")
+    elif calc_minutes > 1:
+        print( str(calc_minutes) +" minutes")
+    elif calc_minutes == 0:
+        print(str(calc_minutes) + " minutes" )
+
+
+
+
+minutes_to_hours(128)
