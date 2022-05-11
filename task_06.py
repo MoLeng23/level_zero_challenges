9acb9b5af202eff4873fbf52ec7a7dd217c44e5d
MoLeng23 <102603821+MoLeng23@users.noreply.github.com>
2022-05-11 13:43:03 +0200

    Update and rename task_0.6.py to task_06.py

--- task_06.py
+++ task_06.py
@@ -1,8 +1,8 @@
 def maximum(num_one, num_two, num_three):
 
-    if num_one > num_two and num_one > num_three:
+    if num_one >= num_two and num_one >= num_three:
         return num_one
-    elif num_two > num_three and num_two > num_one:
+    elif num_two >= num_three and num_two >= num_one:
         return num_two
     else:
         return num_three
