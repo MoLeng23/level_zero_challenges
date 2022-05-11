47a13c56d82897ca854e59b526075e63d2908ad7
MoLeng23 <102603821+MoLeng23@users.noreply.github.com>
2022-05-11 13:46:30 +0200

    Update and rename task_0.8.py to task_08.py

--- task_08.py
+++ task_08.py
@@ -3,18 +3,12 @@ def minutes_to_hours(minutes):
     calc_minutes = minutes % 60
     if calc_hours == 1:
         print(str(calc_hours) +" hour", ",", " ", end="")
-    elif calc_hours > 1:
+    elif calc_hours > 1 and calc_hours == 0:
         print(str(calc_hours) +" hours", ",", " ", end="")
-    elif calc_hours == 0:
-        print(str(calc_hours) + " hours", "," , " ", end="")
     if calc_minutes == 1:
         print(str(calc_minutes) +" minute")
-    elif calc_minutes > 1:
+    elif calc_minutes > 1 and calc_minutes == 0:
         print( str(calc_minutes) +" minutes")
-    elif calc_minutes == 0:
-        print(str(calc_minutes) + " minutes" )
-
-
 
 
 minutes_to_hours(000)
