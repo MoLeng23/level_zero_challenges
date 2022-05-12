525b5b47c8adba4a8a9523ce233fc35428691bd2
MoLeng23 <102603821+MoLeng23@users.noreply.github.com>
2022-05-12 11:30:02 +0200

    Update task_0.9.py

--- task_0.9.py
+++ task_0.9.py
@@ -1,10 +1,12 @@
 def show_vowels(text):
     text = text.lower()
     print("vowels: ", end="")
+    sep = "%s"
     for vowel in "aeiou":
         if vowel in text:
-            print(vowel,end="," )
-   
+            print(sep % vowel, end="")
+            sep= ", %s"
+      
 
 
 show_vowels("Umuzi")
