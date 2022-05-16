905d756fb9701f80b2bd19f2cbdbf0e46a31f1d1
MoLeng23 <102603821+MoLeng23@users.noreply.github.com>
2022-05-16 11:45:58 +0200

    Update task_0_10.py

--- task_0_10.py
+++ task_0_10.py
@@ -2,11 +2,11 @@ def show_vowels(text_one,text_two):
     text_one = set(text_one.lower())
     text_two = set(text_two.lower())
      
-    print("Common letters: ", end="")
+    print("Common letters: ", end=" ")
     sep = "%s"
     for letter in text_one and text_two:
         if letter in text_one and text_two:
-            print(sep % letter, end="" )
+            print(sep % letter, end=" " )
             sep = ",%s"
 
 
